6947a65fb5b603236ec4b2571076196c1456bc64
fiddesidde <fredrik.mellberg@gmail.com>
2021-01-11 07:40:38 +0100

    Add ui-build

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const morgan = require('morgan');
 
 app.use(express.json());
-
+app.use(express.static('build'));
 app.use(
     morgan('tiny', {
         skip: (req, res) => req.method === 'POST',
